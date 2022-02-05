bd07129dec8894996007751742b565886fd1193f
Arthur Avila <arthur.la@hotmail.com>
2022-02-05 13:45:27 -0300

    Started Go microservice

--- working/main.go
+++ working/main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
-		d, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return
-		}
-
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
 
 	http.ListenAndServe(":9090", nil)
 }
